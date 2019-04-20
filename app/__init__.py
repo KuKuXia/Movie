2e90155733d47d8027ae6ea381ccd628387fce14
LongXiaJun <liuxiaolongsdu@mail.sdu.edu.cn>
2019-04-20 20:40:14 +0800

    完成所有页面的搭建。

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@
 # Note:
 
 # coding:utf8
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 app.debug = True
@@ -13,3 +13,9 @@ from app.admin import admin as admin_blueprint
 
 app.register_blueprint(home_blueprint)
 app.register_blueprint(admin_blueprint, url_prefix='/admin')
+
+
+# 404
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("home/404.html"),404
--- app/home/views.py
+++ app/home/views.py
@@ -5,8 +5,76 @@
 # coding:uft8
 
 from . import home
+from flask import render_template, redirect, url_for
 
 
+#首页
 @home.route("/")
 def index():
-    return "<h1 style='color:green'> this is home <h1/>"
+    return render_template("home/index.html")
+
+#首页
+@home.route("/animation/")
+def animation():
+    return render_template("home/animation.html")
+
+
+
+# 登录页面
+@home.route('/login/')
+def login():
+    return render_template("home/login.html")
+
+
+# 退出页面
+@home.route('/logout/')
+def logout():
+    return redirect(url_for("home.login"))
+
+
+# 注册页面
+@home.route('/register/')
+def register():
+    return render_template("home/register.html")
+
+
+# 会员中心
+@home.route('/user/')
+def user():
+    return render_template("home/user.html")
+
+
+# 修改密码
+@home.route('/pwd/')
+def pwd():
+    return render_template("home/pwd.html")
+
+
+# 评论记录
+@home.route('/comments/')
+def comments():
+    return render_template("home/comments.html")
+
+
+# 登录日志
+@home.route('/loginlog/')
+def loginlog():
+    return render_template("home/loginlog.html")
+
+
+# 收藏电影
+@home.route('/moviecol/')
+def moviecol():
+    return render_template("home/moviecol.html")
+
+# 搜索电影
+@home.route('/search/')
+def search():
+    return render_template("home/search.html")
+
+# 电影详情
+@home.route('/play/')
+def play():
+    return render_template("home/play.html")
+
+
