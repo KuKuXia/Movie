f4b34eb73cfbb0f26e76d76e2e6252df2aa38057
LongXiaJun <liuxiaolongsdu@mail.sdu.edu.cn>
2019-04-30 19:33:55 +0800

    部署之前。

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from flask import Flask,render_template
 import os
 import pymysql
 from flask_sqlalchemy import SQLAlchemy
+from flask_redis import FlaskRedis
 
 pymysql.install_as_MySQLdb()
 
@@ -16,11 +17,13 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:MySQL503@127.0.0.1:3306/movie"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 app.config["SECRET_KEY"] = '8c5e7c76e3c042e3b65e3188fdd0e3b1'
+app.config["REDIS_URL"] = "redis://192.168.37.132:6379/0"
+
 app.config["UP_DIR"] = os.path.join(os.path.abspath(os.path.dirname(__file__)), "static/uploads/")
 app.config["FC_DIR"] = os.path.join(os.path.abspath(os.path.dirname(__file__)), "static/uploads/users/")
 app.debug = True
 db = SQLAlchemy(app)
-
+rd = FlaskRedis(app)
 from app.home import home as home_blueprint
 from app.admin import admin as admin_blueprint
 
